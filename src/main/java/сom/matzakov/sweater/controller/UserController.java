ff1428f025e802b4bd4624930afd477b31b6b895
Alexander Macakov <macakovaleksandr@gmail.com>
2018-11-23 16:56:29 +0400

    Добавил возможность подписаться на пользователя

--- src/main/java/сom/matzakov/sweater/controller/UserController.java
+++ src/main/java/сom/matzakov/sweater/controller/UserController.java
@@ -4,6 +4,7 @@ package сom.matzakov.sweater.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -64,6 +65,44 @@ public class UserController {
         userService.updateProfile(user, password, email);
 
         return "redirect:/user/profile";
+    }
+
+
+    @GetMapping("subscribe/{user}")
+    public String subscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ) {
+        userService.subscribe(currentUser, user);
+
+        return "redirect:/user-messages/" + user.getId();
+    }
+
+    @GetMapping("unsubscribe/{user}")
+    public String unsubscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ) {
+        userService.unsubscribe(currentUser, user);
+
+        return "redirect:/user-messages/" + user.getId();
+    }
+
+    @GetMapping("{type}/{user}/list")
+    public String userList(
+            @PathVariable User user,
+            @PathVariable String type,
+            Model model
+    ) {
+        model.addAttribute("userChanel", user);
+        model.addAttribute("type", type);
+
+        if ("subscriptions".equals(type)) {
+            model.addAttribute("users", user.getSubscriptions());
+        } else {
+            model.addAttribute("users", user.getSubscribers());
+        }
+        return "subscriptions";
     }
 
 }
